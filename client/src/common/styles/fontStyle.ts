eb6e1578188a8c70d144c71d8e92aabf6d94f4ea
HJ <jo920208@gmail.com>
2021-03-03 16:35:55 +0900

    🐛 global font style > url 참조 수정

--- client/src/common/styles/fontStyle.ts
+++ client/src/common/styles/fontStyle.ts
@@ -7,8 +7,8 @@ export default css`
     font-style: normal;
     font-weight: 100;
     src: local(""),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-100.woff2") format("woff2"),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-100.woff") format("woff");
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-100.woff2") format("woff2"),
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-100.woff") format("woff");
   }
   /* noto-sans-kr-300 - latin_korean */
   @font-face {
@@ -16,8 +16,8 @@ export default css`
     font-style: normal;
     font-weight: 300;
     src: local(""),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-300.woff2") format("woff2"),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-300.woff") format("woff");
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-300.woff2") format("woff2"),
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-300.woff") format("woff");
   }
   /* noto-sans-kr-regular - latin_korean */
   @font-face {
@@ -25,8 +25,8 @@ export default css`
     font-style: normal;
     font-weight: 400;
     src: local(""),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-400.woff2") format("woff2"),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-400.woff") format("woff");
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-400.woff2") format("woff2"),
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-400.woff") format("woff");
   }
   /* noto-sans-kr-500 - latin_korean */
   @font-face {
@@ -34,8 +34,8 @@ export default css`
     font-style: normal;
     font-weight: 500;
     src: local(""),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-500.woff2") format("woff2"),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-500.woff") format("woff");
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-500.woff2") format("woff2"),
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-500.woff") format("woff");
   }
   /* noto-sans-kr-700 - latin_korean */
   @font-face {
@@ -43,8 +43,8 @@ export default css`
     font-style: normal;
     font-weight: 700;
     src: local(""),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-700.woff2") format("woff2"),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-700.woff") format("woff");
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-700.woff2") format("woff2"),
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-700.woff") format("woff");
   }
   /* noto-sans-kr-900 - latin_korean */
   @font-face {
@@ -52,7 +52,7 @@ export default css`
     font-style: normal;
     font-weight: 900;
     src: local(""),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-900.woff2") format("woff2"),
-      url("fonts/NotoSans/noto-sans-kr-v13-korean-900.woff") format("woff");
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-900.woff2") format("woff2"),
+      url("/fonts/NotoSans/noto-sans-kr-v13-korean-900.woff") format("woff");
   }
 `;
