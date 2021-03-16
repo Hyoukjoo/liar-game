4104668642a27c435d772a6e4344da2f41b4da4a
HJ <jo920208@gmail.com>
2021-03-16 11:15:36 +0900

    🔥 Atom > UnderlineInput 제거

--- client/src/atoms/Input/UnderlineInput/index.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import UnderlineInput from ".";
-export default { title: "Atom/Input" };
-
-export const Underline = () => {
-  return <UnderlineInput placeholder='이름 입력' maxLength={10} />;
-};
