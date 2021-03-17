235d6e63c42cedc822d593fccd991a4ad6497ff3
HJ <jo920208@gmail.com>
2021-03-17 10:22:53 +0900

    ✨ hook > useClient 추가

---
+++ client/src/hooks/useClient.ts
@@ -0,0 +1,26 @@
+import React from "react";
+
+const useClient = () => {
+  const [clientWitdh, setClientWitdh] = React.useState(0);
+  const [clientHeight, setClientHeight] = React.useState(0);
+
+  React.useEffect(() => {
+    setClientWitdh(window.innerWidth);
+    setClientHeight(window.innerHeight);
+
+    const onResize = () => {
+      setClientWitdh(window.innerWidth);
+      setClientHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
+
+  return {
+    clientWitdh,
+    clientHeight,
+  };
+};
+
+export default useClient;
