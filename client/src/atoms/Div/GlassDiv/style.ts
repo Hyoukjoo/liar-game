18076b2ae9862d2632fa05bd6bb075d6c7a91fc7
HJ <jo920208@gmail.com>
2021-04-04 11:10:44 +0900

    💄 Remove backgound

--- client/src/atoms/Div/GlassDiv/style.ts
+++ client/src/atoms/Div/GlassDiv/style.ts
@@ -1,13 +1,11 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
-import { getGlassMorphismStyle } from "@common/styles/glassmorphism";
-import { getWhiteOpacity } from "@common/styles/opacity";
-import BaseDiv from "../BaseDiv";
+import { getGlassMorphismStyle } from '@common/styles/glassmorphism';
+import BaseDiv from '../BaseDiv';
 
 namespace GlassDivStyle {
   export const Div = styled(BaseDiv)`
     ${getGlassMorphismStyle};
-    background: ${getWhiteOpacity(5)};
   `;
 }
 
