49ba26768866bc219a3d7dcec1c9e8c54bd0dc14
HJ <jo920208@gmail.com>
2021-04-04 11:18:25 +0900

    💄 Use color variables

--- client/src/common/styles/storybook.ts
+++ client/src/common/styles/storybook.ts
@@ -1,5 +1,5 @@
-import styled from "@emotion/styled";
-import { white } from "./color";
+import styled from '@emotion/styled';
+import { background1, background2, background3, white } from './color';
 
 namespace CommonStoryBookStyle {
   export const GlassLayout = styled.div`
@@ -10,9 +10,9 @@ namespace CommonStoryBookStyle {
     width: calc(100vw - 20px);
     height: 100vh;
     background-color: ${white};
-    background-image: radial-gradient(at top left, #28527a, transparent),
-      radial-gradient(at top right, #8ac4d0, transparent),
-      radial-gradient(at bottom left, #e4fbff, transparent);
+    background-image: radial-gradient(at top left, ${background1}, transparent),
+      radial-gradient(at top right, ${background2}, transparent),
+      radial-gradient(at bottom left, ${background3}, transparent);
     background-size: 100% 100%;
     background-repeat: no-repeat;
     box-sizing: border-box;
