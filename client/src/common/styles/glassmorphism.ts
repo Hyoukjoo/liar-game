954cb6a05a93f4011852e9331bec7965387bebc5
HJ <jo920208@gmail.com>
2021-04-04 11:15:53 +0900

    💄 Update style - background, border-radius

--- client/src/common/styles/glassmorphism.ts
+++ client/src/common/styles/glassmorphism.ts
@@ -1,15 +1,15 @@
-import { css } from "@emotion/react";
-import { white } from "./color";
-import { getBlackOpacity, getWhiteOpacity } from "./opacity";
+import { css } from '@emotion/react';
+import { white } from './color';
+import { getBlackOpacity, getWhiteOpacity } from './opacity';
 
 export const getGlassMorphismStyle = () => css`
   color: ${white};
-  background: transparent;
+  background: ${getWhiteOpacity(10)};
   box-shadow: 4px 4px 20px 0px ${getBlackOpacity(10)};
   border: none;
   border-top: 1px solid ${getWhiteOpacity(50)};
   border-left: 1px solid ${getWhiteOpacity(50)};
   backdrop-filter: blur(5px);
-  border-radius: 2px;
+  border-radius: 10px;
   outline: none;
 `;
