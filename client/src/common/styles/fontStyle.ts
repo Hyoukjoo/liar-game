a8d4db818f9642bc7a0e4f09d36428aaa87689d9
HJ <jo920208@gmail.com>
2021-04-07 16:41:51 +0900

    🍱 Add font: Jelly Fruit

--- client/src/common/styles/fontStyle.ts
+++ client/src/common/styles/fontStyle.ts
@@ -6,8 +6,8 @@ export default css`
     font-family: NotoSans;
     font-style: normal;
     font-weight: 100;
-    src: local(''),
-      url('/fonts/NotoSans/noto-sans-kr-v13-korean-100.woff2') format('woff2'),
+    src: url('/fonts/NotoSans/noto-sans-kr-v13-korean-100.woff2')
+        format('woff2'),
       url('/fonts/NotoSans/noto-sans-kr-v13-korean-100.woff') format('woff');
   }
   /* noto-sans-kr-300 - latin_korean */
@@ -15,8 +15,8 @@ export default css`
     font-family: NotoSans;
     font-style: normal;
     font-weight: 300;
-    src: local(''),
-      url('/fonts/NotoSans/noto-sans-kr-v13-korean-300.woff2') format('woff2'),
+    src: url('/fonts/NotoSans/noto-sans-kr-v13-korean-300.woff2')
+        format('woff2'),
       url('/fonts/NotoSans/noto-sans-kr-v13-korean-300.woff') format('woff');
   }
   /* noto-sans-kr-regular - latin_korean */
@@ -24,8 +24,8 @@ export default css`
     font-family: NotoSans;
     font-style: normal;
     font-weight: 400;
-    src: local(''),
-      url('/fonts/NotoSans/noto-sans-kr-v13-korean-400.woff2') format('woff2'),
+    src: url('/fonts/NotoSans/noto-sans-kr-v13-korean-400.woff2')
+        format('woff2'),
       url('/fonts/NotoSans/noto-sans-kr-v13-korean-400.woff') format('woff');
   }
   /* noto-sans-kr-500 - latin_korean */
@@ -33,8 +33,8 @@ export default css`
     font-family: NotoSans;
     font-style: normal;
     font-weight: 500;
-    src: local(''),
-      url('/fonts/NotoSans/noto-sans-kr-v13-korean-500.woff2') format('woff2'),
+    src: url('/fonts/NotoSans/noto-sans-kr-v13-korean-500.woff2')
+        format('woff2'),
       url('/fonts/NotoSans/noto-sans-kr-v13-korean-500.woff') format('woff');
   }
   /* noto-sans-kr-700 - latin_korean */
@@ -42,8 +42,8 @@ export default css`
     font-family: NotoSans;
     font-style: normal;
     font-weight: 700;
-    src: local(''),
-      url('/fonts/NotoSans/noto-sans-kr-v13-korean-700.woff2') format('woff2'),
+    src: url('/fonts/NotoSans/noto-sans-kr-v13-korean-700.woff2')
+        format('woff2'),
       url('/fonts/NotoSans/noto-sans-kr-v13-korean-700.woff') format('woff');
   }
   /* noto-sans-kr-900 - latin_korean */
@@ -51,8 +51,13 @@ export default css`
     font-family: NotoSans;
     font-style: normal;
     font-weight: 900;
-    src: local(''),
-      url('/fonts/NotoSans/noto-sans-kr-v13-korean-900.woff2') format('woff2'),
+    src: url('/fonts/NotoSans/noto-sans-kr-v13-korean-900.woff2')
+        format('woff2'),
       url('/fonts/NotoSans/noto-sans-kr-v13-korean-900.woff') format('woff');
+  }
+  /* JellyFruit */
+  @font-face {
+    font-family: JellyFruit;
+    src: url('/fonts/JellyFruit/JellyFruit.otf') format('opentype');
   }
 `;
