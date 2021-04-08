57a7aabe29ee285c60558289305a2997528d86a4
HJ <jo920208@gmail.com>
2021-04-08 20:37:53 +0900

    💄 Update BaseSection: padding 30px -> 20px

--- client/src/components/atoms/Section/BaseSection/style.ts
+++ client/src/components/atoms/Section/BaseSection/style.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 namespace BaseSectionStyle {
   export const Section = styled.section`
-    padding: 30px;
+    padding: 20px;
     max-width: 375px;
     width: 100%;
   `;
