9e8946e3e721ee9a4c7d28100a7b99ee255687cf
HJ <jo920208@gmail.com>
2021-04-08 14:30:29 +0900

    ✅ Add e2e test: Home view

--- client/cypress/integration/views/Home/index.spec.ts
+++ client/cypress/integration/views/Home/index.spec.ts
@@ -1,14 +1,58 @@
-context('Test', () => {
-  describe('Home ', () => {
+context('Home View Test', () => {
+  describe('In Not Login ', () => {
     beforeEach(() => {
       cy.visit('/');
     });
-    it('Title has the text', () => {
-      cy.get('h1').should('have.text', 'Test Hello World');
+
+    it('should show nickname that is 로그인이 필요합니다.', () => {
+      cy.get('p').should('have.text', '로그인이 필요합니다.');
     });
 
-    it('Its test', () => {
-      cy.get('body').its('');
+    it('should has one button in not login', () => {
+      cy.get('button').should('have.length', 1);
+    });
+
+    it('should move to login page on click login button', () => {
+      cy.get('button').first().click();
+
+      cy.location('pathname').should('equal', '/account/login');
+    });
+  });
+
+  describe('In Login', () => {
+    beforeEach(() => {
+      cy.login();
+      cy.visit('/');
+    });
+
+    afterEach(() => {
+      cy.getCookie('AUTH_TOKEN').then((cookie) => {
+        if (cookie !== null) {
+          cy.logout();
+        }
+      });
+    });
+
+    it('should has three buttons in login', () => {
+      cy.get('button').should('have.length', 3);
+    });
+
+    it('should move to create room page on click create room button', () => {
+      cy.get('button').first().click();
+
+      cy.location('pathname').should('equal', '/room/create');
+    });
+
+    it('should move to join room page on click join room button', () => {
+      cy.get('button').first().next().click();
+
+      cy.location('pathname').should('equal', '/room/join');
+    });
+
+    it('should logout on click logout button', () => {
+      cy.get('button').last().click();
+
+      cy.getCookie('AUTH_TOKEN').should('equal', null);
     });
   });
 });
