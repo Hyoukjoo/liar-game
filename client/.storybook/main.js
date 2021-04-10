f63ec3cd2d34609e303e92a82744e797c5eb65ab
HJ <jo920208@gmail.com>
2021-04-10 14:21:53 +0900

    ➕ Add storybook addon-actions

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-knobs',
-    '@storybook/addon-storysource',
+    '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
   webpackFinal: (config) => {
