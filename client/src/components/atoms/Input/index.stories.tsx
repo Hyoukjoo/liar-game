b3f09412aec934318fdf9d83fc31566c21de7831
HJ <jo920208@gmail.com>
2021-04-13 11:22:23 +0900

    ✅ Update storybook: ATOM/INPUT

--- client/src/components/atoms/Input/index.stories.tsx
+++ client/src/components/atoms/Input/index.stories.tsx
@@ -1,13 +1,19 @@
-import { BaseInput, TextInput } from '.';
+import { BaseInput, GlassInput } from '.';
+import { white } from '@common/styles/color';
 
 export default {
   title: 'ATOM/INPUT',
 };
 
 export const Base_Input = () => {
-  return <BaseInput />;
+  return (
+    <BaseInput
+      placeholder={'여기 있어요~'}
+      style={{ border: `2px dashed ${white} !important;` }}
+    />
+  );
 };
 
-export const Text_Input = () => {
-  return <TextInput />;
+export const Glass_Input = () => {
+  return <GlassInput />;
 };
