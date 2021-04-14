66c0d02cfa10e524ab2070efa1e062a5edd60b6e
HJ <jo920208@gmail.com>
2021-04-14 17:31:28 +0900

    💄 Add Icon: RightArrowFat

---
+++ client/src/components/atoms/Icon/RightArrowFat.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+function SvgRightArrowFat(props) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width={45.513}
+      height={45.512}
+      viewBox='0 0 45.513 45.512'
+      {...props}
+    >
+      <path d='M44.275 19.739L30.211 5.675a3.175 3.175 0 00-5.419 2.251l.015 5.903-21.64.054A3.175 3.175 0 000 17.065l.028 11.522a3.172 3.172 0 003.182 3.167l21.64-.054.014 5.899a3.176 3.176 0 005.431 2.228L44.29 25.694a4.218 4.218 0 00-.015-5.955z' />
+    </svg>
+  );
+}
+
+export default SvgRightArrowFat;
