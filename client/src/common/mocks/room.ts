6d1e81e8bed434fb7f8cf90bdccbf793bca2315f
HJ <jo920208@gmail.com>
2021-04-14 17:31:40 +0900

    🌱 Add room seed

---
+++ client/src/common/mocks/room.ts
@@ -0,0 +1,82 @@
+import RoomModel from '@models/RoomModel';
+
+export const ROOMS = [
+  new RoomModel({
+    id: '0',
+    name: '자양동에서 라이어를 찾는 사람들이 ㅋㅋㅋ',
+    owner: {
+      id: 0,
+      email: 'test@test.com',
+      nickname: 'test',
+      createdAt: '2021-4-12',
+      updatedAt: '2021-4-12',
+    },
+    createdAt: '2021-4-14',
+    updatedAt: '2021-4-14',
+  }),
+  new RoomModel({
+    id: '1',
+    name: 'test room1',
+    owner: {
+      id: 1,
+      email: 'test1@test.com',
+      nickname: 'test1',
+      createdAt: '2021-4-12',
+      updatedAt: '2021-4-12',
+    },
+    createdAt: '2021-4-14',
+    updatedAt: '2021-4-14',
+  }),
+  new RoomModel({
+    id: '2',
+    name: 'test room2',
+    owner: {
+      id: 2,
+      email: 'test2@test.com',
+      nickname: 'test2',
+      createdAt: '2021-4-12',
+      updatedAt: '2021-4-12',
+    },
+    createdAt: '2021-4-14',
+    updatedAt: '2021-4-14',
+  }),
+  new RoomModel({
+    id: '3',
+    name: 'test room3',
+    owner: {
+      id: 3,
+      email: 'test3@test.com',
+      nickname: 'test3',
+      createdAt: '2021-4-12',
+      updatedAt: '2021-4-12',
+    },
+    createdAt: '2021-4-14',
+    updatedAt: '2021-4-14',
+  }),
+  new RoomModel({
+    id: '4',
+    name: 'test room4',
+    owner: {
+      id: 4,
+      email: 'test4@test.com',
+      nickname: 'test4',
+      createdAt: '2021-4-12',
+      updatedAt: '2021-4-12',
+    },
+    createdAt: '2021-4-14',
+    updatedAt: '2021-4-14',
+  }),
+  new RoomModel({
+    id: '5',
+    name: 'test room5',
+    owner: {
+      id: 5,
+      email: 'test5@test.com',
+      nickname: 'test5',
+      createdAt: '2021-4-12',
+      updatedAt: '2021-4-12',
+    },
+    createdAt: '2021-4-14',
+    updatedAt: '2021-4-14',
+  }),
+];
