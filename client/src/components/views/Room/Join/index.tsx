19fd19b194052709e797fd4bdaa427a2d576dcdf
HJ <jo920208@gmail.com>
2021-04-14 17:43:02 +0900

    💄 Update RoomJoinView

--- client/src/components/views/Room/Join/index.tsx
+++ client/src/components/views/Room/Join/index.tsx
@@ -1,24 +1,21 @@
-import { FC, useCallback, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useRouter } from 'next/dist/client/router';
 
-import Style from './style';
-import RoomItem from '../components/RoomItem';
-import { GlassInput } from '@atoms/Input';
-import { GlassButton } from '@atoms/Button';
-import { Refresh } from '@atoms/Icon';
-import { IconButton } from '@molecules/Button';
 import { HeaderLayout } from '@templates/Layout';
 import useSocket from '@hooks/useSocket';
 import RoomModel from '@models/RoomModel';
-import { getRoomList, findRooms, joinRoom } from '@services/Room/remotes';
+import {
+  getRoomList,
+  findRooms,
+  joinRoom as joinRoomRequest,
+} from '@services/Room/remotes';
+import JoinCard from '@templates/Card/JoinCard';
 
 const RoomJoinView: FC = ({}) => {
   const router = useRouter();
   const { socket, roomSocket } = useSocket();
 
-  const [searchValue, setSearchValue] = useState('');
-  const [rooms, setRooms] = useState<RoomModel[]>();
-  const [selectedRoomId, setSelectedRoomId] = useState<string>();
+  const [rooms, setRooms] = useState<RoomModel[]>([]);
 
   useEffect(() => {
     getRoomList().then((results) => {
@@ -27,77 +24,45 @@ const RoomJoinView: FC = ({}) => {
     });
   }, []);
 
-  const onClickSearchButton = async () => {
-    if (searchValue.trim().length < 1) return;
-    const result = await findRooms(searchValue);
+  const searchRoom = async (value: string) => {
+    if (value.trim().length < 1) return;
+
+    const result = await findRooms(value);
 
     const roomModels = RoomModel.createFromApi(result);
 
     setRooms(roomModels);
   };
 
-  const onClickRoomItem = (roomId: string) => {
-    setSelectedRoomId(roomId);
+  const joinRoom = async (roomId: string) => {
+    const userId = Number(router.query.userId);
+    const result = await joinRoomRequest({ userId, roomId });
+
+    if (result !== null) {
+      router.push({
+        pathname: `/game/bench`,
+        query: {
+          roomId,
+          userId,
+        },
+      });
+    }
   };
 
-  const onClickInitButton = async () => {
-    setSearchValue('');
-    setRooms(null);
-    setSelectedRoomId(null);
+  const refreshRoomList = async () => {
     const result = await getRoomList();
     const roomModels = RoomModel.createFromApi(result);
     setRooms(roomModels);
   };
 
-  const onClickJoinButton = async () => {
-    const userId = Number(router.query.userId);
-    const result = await joinRoom({ userId, roomId: selectedRoomId });
-
-    router.push({
-      pathname: `/game/bench`,
-      query: {
-        roomId: selectedRoomId,
-        userId: userId,
-      },
-    });
-  };
-
-  const RoomItems = useCallback(
-    () => (
-      <Style.Ul>
-        {rooms?.map((room) => (
-          <RoomItem
-            key={room.id}
-            roomName={room.name}
-            ownerName={room.owner.nickname}
-            onClick={() => onClickRoomItem(room.id)}
-            isSelected={selectedRoomId === room.id}
-          />
-        ))}
-      </Style.Ul>
-    ),
-    [rooms, selectedRoomId, searchValue]
-  );
-
   return (
     <HeaderLayout title='방 참여하기'>
-      <Style.Container>
-        <Style.SearchBox>
-          <GlassInput
-            placeholder='방 찾기'
-            value={searchValue}
-            onChange={(e) => {
-              setSearchValue(e.target.value);
-            }}
-          />
-          <GlassButton onClick={onClickSearchButton}>검색</GlassButton>
-        </Style.SearchBox>
-        <RoomItems />
-        <Style.Bottom>
-          <GlassButton onClick={onClickJoinButton}>참여하기</GlassButton>
-          <IconButton onClick={onClickInitButton} Svg={Refresh} />
-        </Style.Bottom>
-      </Style.Container>
+      <JoinCard
+        rooms={rooms}
+        searchRoom={searchRoom}
+        joinRoom={joinRoom}
+        refreshRoomList={refreshRoomList}
+      />
     </HeaderLayout>
   );
 };
